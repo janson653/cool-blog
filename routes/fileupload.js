8e17f39a6ac95ea48364a0e697525b0752fc362a
janson <zhangqian653@163.com>
2017-04-28 22:02:18 +0800

    修改文件上传目录

--- routes/fileupload.js
+++ routes/fileupload.js
@@ -10,7 +10,7 @@ router.post('/', function (req, res, next) {
     console.log('fileupload api')
     var form = new formidable.IncomingForm(); //创建上传表单
     form.encoding = 'utf-8'; //设置编辑
-    form.uploadDir = './fileupload'; //设置上传目录
+    form.uploadDir = './fileupload/static'; //设置上传目录
     form.keepExtensions = true; //保留后缀
     form.maxFieldsSize = 1024 * 1024 * 1024;   //文件大小 k
     form.parse(req, function (err, fields, files) {
