cda6a843e709fb674df5667e449a14913a8492bd
janson <zhangqian653@163.com>
2017-05-11 23:13:54 +0800

    add mongodb auth

--- app.js
+++ app.js
@@ -53,16 +53,14 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send({
+        message: err.message,
+        error: err
+    });
+    return;
 });
-
 
 
 module.exports = app;
